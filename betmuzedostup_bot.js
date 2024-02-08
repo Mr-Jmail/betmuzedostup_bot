b13aaff538d5ce392511b0bcdd74cf401a626f24
Jmail <gamelelkazzaz@gmail.com>
2024-02-08 12:26:28 +0300

    updated chatIdToSend

--- betmuzedostup_bot.js
+++ betmuzedostup_bot.js
@@ -2,8 +2,8 @@ const path = require("path")
 require("dotenv").config({path: path.join(__dirname, ".env")})
 const { Telegraf, Scenes, session } = require("telegraf")
 const bot = new Telegraf(process.env.botToken)
-// const chatIdToSendMessage = -4096151300
-const chatIdToSendMessage = 1386450473
+const chatIdToSendMessage = -4096151300
+// const chatIdToSendMessage = 1386450473
 const { getLink, updateLink } = require("./functions")
 
 const updateLinkScene = require("./updateLinkScene")
@@ -23,13 +23,11 @@ bot.command("updateLink", ctx => ctx.scene.enter("updateLinkScene"))
 bot.command("getId", ctx => ctx.reply(ctx.chat.id.toString()).catch(err => console.log(err)))
 
 setInterval(async() => {
-// ;(async function() {
     const url = getLink()
     if(url == "") return
     if(!await siteIsBlocked(url)) return
     await bot.telegram.sendMessage(chatIdToSendMessage, `⛔️Site "${url}" was blocked. To update the link use /updateLink`).catch(err => console.log(err))
     updateLink("")
-// })()
 }, 1000 * 60 * 5);
 
 bot.launch()
